fefb40928a4ccda53e289f231569f1bfdc26254d
URK21CS1147KUSHMITHA <109952808+URK21CS1147KUSHMITHA@users.noreply.github.com>
2022-08-22 15:36:11 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,123 +1,123 @@
 class Solution:
- """This class implements linear queue.
- Attributes:
- stack: A list which maintains the content of stack.
- queue: A list which maintains the content of queue.
- top: An integer which denotes the index of the element at the top of the stack.
- front: An integer which denotes the index of the element at the front of the queue.
- rear: An integer which denotes the index of the element at the rear of the queue.
- size: An integer which represents the size of stack and queue.
- """
+    """This class implements linear queue.
+      Attributes:
+          stack: A list which maintains the content of stack.
+          queue: A list which maintains the content of queue.
+          top: An integer which denotes the index of the element at the top of the stack.
+          front: An integer which denotes the index of the element at the front of the queue.
+          rear: An integer which denotes the index of the element at the rear of the queue.
+          size: An integer which represents the size of stack and queue.
+      """
 
- # Write your code here
- def __init__(self, size):
- """Inits Solution with stack, queue, size, top, front and rear.
- Arguments:
- size: An integer to set the size of stack and queue.
- """
- self.stack = []
- self.queue = []
- self.size = size
- self.top = -1
- self.rear = -1
- self.front = -1
+    # Write your code here
+    def __init__(self, size):
+        """Inits Solution with stack, queue, size, top, front and rear.
+        Arguments:
+          size: An integer to set the size of stack and queue.
+        """
+        self.stack = []
+        self.queue = []
+        self.size = size
+        self.top = -1
+        self.rear = -1
+        self.front = -1
 
- def is_stack_empty(self):
- """
- Check whether the stack is empty.
- Returns:
- True if it is empty, else returns False.
- """
- # Write your code here
- if self.top == -1:
- return True
- else:
- return False
+    def is_stack_empty(self):
+        """
+        Check whether the stack is empty.
+        Returns:
+          True if it is empty, else returns False.
+        """
+        # Write your code here
+        if self.top == -1:
+            return True
+        else:
+            return False
 
- def is_queue_empty(self):
- """
- Check whether the queue is empty.
- Returns:
- True if it is empty, else returns False.
- """
- # Write your code here
- if self.rear == -1 and self.front == -1:
- return True
- else:
- return False
+    def is_queue_empty(self):
+        """
+        Check whether the queue is empty.
+        Returns:
+          True if it is empty, else returns False.
+        """
+        # Write your code here
+        if self.rear == -1 and self.front == -1:
+            return True
+        else:
+            return False
 
- def is_stack_full(self):
- """
- Check whether the stack is full.
- Returns:
- True if it is full, else returns False.
- """
- # Write your code here
- if self.size == len(self.stack):
- return True
- else:
- return False
+    def is_stack_full(self):
+        """
+        Check whether the stack is full.
+        Returns:
+          True if it is full, else returns False.
+        """
+        # Write your code here
+        if self.size == len(self.stack):
+            return True
+        else:
+            return False
 
- def is_queue_full(self):
- """
- Check whether the queue is full.
- Returns:
- True if it is full, else returns False.
- """
- # Write your code here
- if self.size == len(self.queue):
- return True
- else:
- return False
+    def is_queue_full(self):
+        """
+        Check whether the queue is full.
+        Returns:
+          True if it is full, else returns False.
+        """
+        # Write your code here
+        if self.size == len(self.queue):
+            return True
+        else:
+            return False
 
- def push_character(self, character):
- """
- Push the character to stack, if stack is not full.
- Arguments:
- character: A character that will be pushed to the stack.
- """
- # Write your code here
- if not self.is_stack_full():
- self.stack.append(character)
- self.top += 1
- 
- def enqueue_character(self, character):
- """
- Enqueue the character to queue, if queue is not full.
- Arguments:
- character: A character that will be enqueued to queue.
- """
- # Write your code here
- if not self.is_queue_full():
- self.queue.append(character)
- self.rear += 1
- if self.front == -1:
- self.front += 1
+    def push_character(self, character):
+        """
+        Push the character to stack, if stack is not full.
+        Arguments:
+            character: A character that will be pushed to the stack.
+        """
+        # Write your code here
+        if not self.is_stack_full():
+            self.stack.append(character)
+            self.top += 1
+                    
+    def enqueue_character(self, character):
+        """
+        Enqueue the character to queue, if queue is not full.
+        Arguments:
+            character: A character that will be enqueued to queue.
+        """
+        # Write your code here
+        if not self.is_queue_full():
+            self.queue.append(character)
+            self.rear += 1
+            if self.front == -1:
+                self.front += 1
 
- def pop_character(self):
- """
- Do pop operation if the stack is not empty.
- Returns:
- The data that is popped out if the stack is not empty.
- """
- # Write your code here
- if not self.is_stack_empty():
- data = self.stack.pop()
- self.top -= 1
- return data
+    def pop_character(self):
+        """
+        Do pop operation if the stack is not empty.
+        Returns:
+          The data that is popped out if the stack is not empty.
+        """
+        # Write your code here
+        if not self.is_stack_empty():
+            data = self.stack.pop()
+            self.top -= 1
+            return data
 
- def dequeue_character(self):
- """
- Do dequeue operation if the queue is not empty.
- Returns:
- The data that is dequeued if the queue is not empty.
- """
- # Write your code here
- if not self.is_queue_empty():
- data = self.queue[self.front]
- self.front += 1
- return data
- 
+    def dequeue_character(self):
+        """
+        Do dequeue operation if the queue is not empty.
+        Returns:
+          The data that is dequeued if the queue is not empty.
+        """
+        # Write your code here
+        if not self.is_queue_empty():
+            data = self.queue[self.front]
+            self.front += 1
+            return data
+        
 
 
 # read the string text
@@ -131,9 +131,9 @@ solution = Solution(length_of_text)
 
 # push/enqueue all the characters of string text to stack
 for index in range(length_of_text):
- # Write code here
- solution.push_character(text[index])
- solution.enqueue_character(text[index])
+    # Write code here
+    solution.push_character(text[index])
+    solution.enqueue_character(text[index])
 
 is_palindrome = True
 '''
@@ -144,12 +144,12 @@ If the comparison fails, set is_palindrome as False.
 '''
 # Write the necessary logic
 for index in range(length_of_text):
- if solution.pop_character() != solution.dequeue_character():
- is_palindrome = False
- break
+    if  solution.pop_character() != solution.dequeue_character():
+        is_palindrome = False
+        break
 
 # finally print whether string text is palindrome or not.
 if is_palindrome:
- print("The word, " + text + ", is a palindrome.");
+    print("The word, " + text + ", is a palindrome.");
 else:
- print("The word, " + text + ", is not a palindrome.")
+    print("The word, " + text + ", is not a palindrome.")
